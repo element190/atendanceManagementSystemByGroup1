82a29916dad46bcec2159b25033a0a27f8df3cd7
Chinedu Ugbo <o.ugbochinedu@yahoo.com>
2023-10-04 19:14:11 +0100

    updated for deployment

--- Attendance/src/features/UI/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-// import classes from "../button/button.module.css"
-
-const Button = (props)=>{
-    return <button onClick={props.onClick} className={`${props.className}`}>{props.children}</button>;
-};
-
-export default Button;
